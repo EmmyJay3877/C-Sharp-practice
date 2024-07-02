a5556e04e2789609286a75cf2e2401466198c781
emmyjay387 <104701190+emmyjay387@users.noreply.github.com>
2024-07-02 15:35:39 +0100

    Add project files.

---
+++ AccessModifierDemo/ConsoleUI/InheritedDemo.cs
@@ -0,0 +1,19 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class InheritedDemo : AccessDemo
+    {
+
+        public void Demo()
+        {
+            ProtectedDemo();
+            ProtectedInternalDemo();
+        }
+    }
+}
