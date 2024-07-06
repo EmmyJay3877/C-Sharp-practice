ff7f9f1b2a58cb67aa14500b7a8ff1f4e40d00de
emmyjay387 <104701190+emmyjay387@users.noreply.github.com>
2024-07-06 13:26:31 +0100

    IntefaceDemo

---
+++ InterfaceDemo/ConsoleUI/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IProductModel> cart = AddSampleData();
+
+            CustomerModel customer = GetCustomer();
+
+            foreach (IProductModel prod in cart)
+            {
+                prod.ShipItem(customer);
+
+                if (prod is IDigitalProductModel digital)
+                {
+                    Console.WriteLine($"For the {digital.Title}, you have {digital.TotalDownloadsLeft} downloads left");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        private static CustomerModel GetCustomer()
+        {
+            return new CustomerModel
+            {
+                FirstName = "Sam",
+                LastName = "John",
+                EmailAddress = "John@gmail.com",
+                City = "LA",
+                PhoneNumber = "1234567890",
+            };
+        }
+
+        private static List<IProductModel> AddSampleData()
+        {
+            List<IProductModel> output = new List<IProductModel>();
+
+            output.Add(new PhysicalProductModel { Title = "Football"});
+            output.Add(new PhysicalProductModel { Title = "T-shirt"});
+            output.Add(new PhysicalProductModel { Title = "Hard Drive"});
+            output.Add(new DigitalProductModel { Title = "C# course" });
+
+            return output;
+        }
+    }
+}
---
+++ InterfaceDemo/DemoLibrary/PhysicalProductModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class PhysicalProductModel : IProductModel
+    {
+
+        public string Title { get; set; }
+        public bool HasBeenDelivered { get; set; }
+        public void ShipItem(CustomerModel customer)
+        {
+            if (HasBeenDelivered == false)
+            {
+                Console.WriteLine($"Simulating shipping {Title} to {customer.FirstName} in {customer.City}");
+                HasBeenDelivered = true;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+
+}
